7a8f70765a20ac53d89fa7cb78c2a0f2799571be
alexbesch26 <beal1031@h-ka.de>
2024-06-27 11:22:24 +0200

    ui finished

--- src/main/java/artcreator/gui/CreatorFrame.java
+++ src/main/java/artcreator/gui/CreatorFrame.java
@@ -1,16 +1,22 @@
 package artcreator.gui;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class CreatorFrame extends JFrame {
-    private JButton button1;
-    private JButton button2;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JSlider slider1;
-    private JTextField textField3;
+    private JButton ImageImportButton;
+    private JButton vorlageDruckenButton;
+    private JTextField a210mmTextField;
+    private JTextField a297mmTextField;
+    private JSlider gridSizeSlider;
+    private JTextField gridSizetextField;
     private JLabel settingsLabel;
     private JPanel creatorJFrame;
     private JPanel vorlagengrößePanel;
@@ -18,16 +24,49 @@ public class CreatorFrame extends JFrame {
     private JPanel InputPanel;
     private JPanel TemplatePenel;
     private JButton reset_btn;
-    private JRadioButton sdfRadioButton;
-    private JRadioButton radioButton2;
-    private JRadioButton radioButton3;
-    private JRadioButton radioButton4;
+    private JRadioButton radioButton0Grad;
+    private JRadioButton radioButton45Grad;
+    private JRadioButton radioButton180Grad;
+    private JRadioButton radioButton270Grad;
+    private JButton createButton;
+    private JTextField markSizeTextField;
+    private JSlider markSizeSlider;
 
     public CreatorFrame() {
-        button1.addActionListener(new ActionListener() {
+        ImageImportButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                System.out.println("ImageImportButton");
+            }
+        });
 
+        vorlageDruckenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("vorlageDruckenButton");
+
+            }
+        });
+        reset_btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("reset_btn");
+
+            }
+        });
+        markSizeSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider source = (JSlider) e.getSource();
+                markSizeTextField.setText(String.valueOf(source.getValue()) + "mm");
+            }
+        });
+        gridSizeSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider source = (JSlider) e.getSource();
+                gridSizetextField.setText(String.valueOf(source.getValue()) + "mm");
+                markSizeSlider.setMaximum(source.getValue());
             }
         });
     }
@@ -36,7 +75,7 @@ public class CreatorFrame extends JFrame {
         CreatorFrame form = new CreatorFrame();
         form.setContentPane(form.creatorJFrame);
         form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        form.setSize(2000, 800);
+        form.setSize(1000, 800);
         form.setVisible(true);
     }
 
